2e7a996e5e13877bf9748f24ce4d4847926985f4
Shashank Bansal <shashank.bansal@hotmail.com>
2022-06-11 12:07:16 +0530

    Intent filters

--- app/src/main/java/edu/shashank/ourfirstapp/Intent1.java
+++ app/src/main/java/edu/shashank/ourfirstapp/Intent1.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Intent1 extends AppCompatActivity {
 
     Button intentButton;
+    EditText  textBox, textbox1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +20,32 @@ public class Intent1 extends AppCompatActivity {
         setContentView(R.layout.activity_intent1);
 
         intentButton = findViewById(R.id.intent1button);
+        textBox = findViewById(R.id.intent1TextBox);
+        textbox1 = findViewById(R.id.intent1Textbox2);
 
         intentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( view.getContext() , Intent2.class );
+                intent.putExtra("text", textBox.getText().toString());
+                intent.putExtra("text1", textbox1.getText().toString());
+
                 startActivity(intent);
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        textBox.setText("");
+        textbox1.setText("");
     }
 }
--- app/src/main/java/edu/shashank/ourfirstapp/Intent2.java
+++ app/src/main/java/edu/shashank/ourfirstapp/Intent2.java
@@ -2,13 +2,39 @@ package edu.shashank.ourfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Intent2 extends AppCompatActivity {
+
+    TextView text,text1;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent2);
+
+        button = findViewById(R.id.intent2button);
+        text = findViewById(R.id.intent2Text);
+        text1 = findViewById(R.id.intent2textbox2);
+
+        String intentValue = getIntent().getExtras().getString("text");
+        String intentValue1 = getIntent().getExtras().getString("text1");
+
+        text.setText(intentValue);
+        text1.setText(intentValue1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Intent1.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
